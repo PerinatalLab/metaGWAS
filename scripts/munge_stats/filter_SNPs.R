2d9c1fa2015700bc40ef4e34101c1c2acad5ce82
psnavais <psnavais@gmail.com>
2021-10-26 11:07:17 +0200

    QC individual sumstats

--- scripts/munge_stats/filter_SNPs.R
+++ scripts/munge_stats/filter_SNPs.R
@@ -1,16 +1,31 @@
 library(data.table)
 library(dplyr)
 
-d= fread(snakemake@input[[1]])
 
-x1= nrow(d)
+hrc= fread(snakemake@input[[1]])
+
+
+funk= function(infile){
+d= fread(infile)
+
+print(paste('Filtering file: ', infile))
 
 d= arrange(d, CHR, POS, EFF, REF)
 
-hrc= fread(snakemake@input[[2]], header=T)
+d= filter(d, pvalue< 1, pvalue>0)
+
+d$pval= pnorm(-abs(d$BETA / d$SE)) * 2
+
+d= filter(d, (abs(-log10(pvalue) - -log10(pval)) / -log10(pval)) * 100 <= 10)
+
+d$ID= with(d, ifelse(REF> EFF, paste(CHR, POS, EFF, REF, sep= ':'), paste(CHR, POS, REF, EFF, sep= ':')))
+
+d$SNP= with(d, ifelse(grepl('I', ID), paste(ID, 'INDEL', sep= ':'), paste(ID, 'SNP', sep= ':')))
+
+
+print(str(d))
 
 d= inner_join(d, hrc, by= 'ID')
-rm(hrc)
 d$EAF= ifelse(is.na(d$EAF), d$eaf, d$EAF)
 
 d$BETA= ifelse(d$REF> d$EFF, -1 * d$BETA, d$BETA)
@@ -19,21 +34,60 @@ d$EAF= ifelse(d$REF> d$EFF, 1 - d$EAF, d$EAF)
 d[d$REF>d$EFF, c("REF", "EFF")]= d[d$REF > d$EFF, c("EFF", "REF")]
 
 d$MAF= ifelse(d$EAF>0.5, 1- d$EAF, d$EAF)
+d= filter(d, MAF>= 0.005)
 
-d= filter(d, pvalue>0, pvalue<1, MAF>0.005, SE>0)
+d= filter(d, pvalue>0, pvalue<1, MAF>=0.005, SE>0)
 
 d= filter(d, (MAF * 2 * N) > 6)
 
 d$maf= ifelse(d$eaf> 0.5, 1 - d$eaf, d$eaf)
 
-d= filter(d, abs(MAF - maf) < 0.2)
+d= filter(d, abs(maf - MAF) < 0.2)
 
-d= select(d, -c(MAF, ID, eaf))
+if (grepl('GAraw/Viva', infile)){
+
+
+
+d$EAF= with(d, ifelse(abs(eaf - EAF)> 0.2, 1 - EAF, EAF))
+d$BETA= with(d, ifelse(abs(eaf - EAF)> 0.2, -1 * BETA, BETA))
+}
+
+
+if (grepl('GAnrm/Viva', infile)){
+
+
+
+d$EAF= with(d, ifelse(abs(eaf - EAF)> 0.2, 1 - EAF, EAF))
+d$BETA= with(d, ifelse(abs(eaf - EAF)> 0.2, -1 * BETA, BETA))
+}
+
+if (grepl('postTerm/HUNT', infile)){
+
+
+
+d$EAF= with(d, ifelse(abs(eaf - EAF)> 0.2, 1 - EAF, EAF))
+d$BETA= with(d, ifelse(abs(eaf - EAF)> 0.2, -1 * BETA, BETA))
+}
+
+
+d= arrange(d, pvalue)
+d= filter(d, !duplicated(ID))
+
+
+d= select(d, -c(MAF, ID, eaf, pval))
 
 x2= nrow(d)
 
-write.table(d, snakemake@output[[1]], col.names= T, row.names=F, sep= '\t', quote= F)
+d$STRAND= '+'
 
-cohort= gsub('_temp.txt', '', unlist(strsplit(snakemake@input[[1]], '/'))[[9]])
+#outfile= paste0(snakemake@params[[1]], gsub('_temp.txt', '', unlist(strsplit(infile, '/'))[9]), '.txt')
 
-cat(c(cohort, '\t', x1, '\t', x2, '\n'), file= snakemake@output[[2]])
+
+fwrite(d, snakemake@output[[1]], sep= '\t')
+}
+
+
+#input_files= snakemake@input[grepl('sumstats', snakemake@input)]
+
+lapply(snakemake@input[[2]], funk)
+
