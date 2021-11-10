d047cabc01f0e7c85ce371597610b4756286dee2
psnavais <psnavais@gmail.com>
2021-11-10 22:31:47 +0100

    vertical dendro

--- scripts/figures/effect_origin_dendrogram.R
+++ scripts/figures/effect_origin_dendrogram.R
@@ -69,12 +69,13 @@ d$clust= clust
 
 circ1 <- ggtree(hc, layout = "circular")
 df1= as.data.frame(select(d, beta_h2, beta_h1, beta_h3))
-names(df1)= c('MnT', 'MT', 'PT')
+#names(df1)= c('MnT', 'MT', 'PT')
+names(df1)= c('Maternal\nnon-transmitted', 'Maternal\ntransmitted', 'Paternal\ntransmitted')
 
 rownames(df1) <- d$rsid_lab
-
+print('
 circ1= ggtree(hc) +
-theme(legend.position='none') +
+theme(legend.position="none") +
 geom_hilight(node=21, fill= colorBlindBlack8[7], alpha=0.5) +
 geom_cladelabel(21, "Mt", offset= 0.03, barsize=0, angle=90, offset.text= -1/100, hjust=0.5, fontsize=8/.pt) +
 geom_hilight(node=24, fill= colorBlindBlack8[3], alpha=0.5) +
@@ -83,27 +84,65 @@ geom_hilight(node=27, fill=colorBlindBlack8[8], alpha=0.5) +
 geom_cladelabel(27, "Maternal", offset=0.03, barsize=0, angle=90, offset.text=-1/100, hjust=0.5, fontsize=8/.pt) +
 geom_hilight(node=26, fill="grey", alpha=0.5)  +
 geom_cladelabel(26, "Unclassified", offset=0.03, barsize=0, angle=90, offset.text=-1/100, hjust=0.5, fontsize=8/.pt) +
-theme(plot.margin=margin(0, 0, 0, 0, unit= 'cm'))+
+theme(plot.margin=margin(0, 0, 0, 0, unit= "cm"))+
 scale_x_continuous(expand= c(0, NA), limits= c(0, 3.1))
 
 grp= split(d$rsid_lab, d$clust)
 max_beta= max(abs(as.numeric(stack(d)$values)))
  
 
-p1= groupOTU(circ1, grp, 'RSID') + 
+p1= groupOTU(circ1, grp, "RSID") + 
 aes(color= RSID ) + 
 theme(legend.position="none", 
-plot.margin=margin(0, 0, 0, 0, unit= 'cm')) +
-scale_colour_manual(values=c(colorBlindBlack8[c(1, 8, 3)], 'grey', colorBlindBlack8[7]), guide= 'none')
+plot.margin=margin(0, 0, 0, 0, unit= "cm")) +
+scale_colour_manual(values=c(colorBlindBlack8[c(1, 8, 3)], "grey", colorBlindBlack8[7]), guide= "none")
 
 
 ctree= gheatmap(p1, df1, offset= 0, width=0.7, 
         colnames=TRUE, legend_title="genotype", colnames_angle= 0, font.size= 8/.pt, hjust= 0.5) +
     geom_tiplab(align = TRUE, linesize=0, offset= 0.9, size= 8/.pt) +
-    scale_fill_gradient2(low= colorBlindBlack8[2], high= colorBlindBlack8[4], mid= 'white', guide= F, limits= c(-max_beta, max_beta)) +
-coord_cartesian(clip = 'off') + 
-  theme(plot.margin=margin(0, 0, 0, 0, unit= 'cm'))
+    scale_fill_gradient2(low= colorBlindBlack8[2], high= colorBlindBlack8[4], mid= "white", guide= F, limits= c(-max_beta, max_beta)) +
+coord_cartesian(clip = "off") + 
+  theme(plot.margin=margin(0, 0, 0, 0, unit= "cm"))
 
-ggsave(snakemake@output[[1]], plot= ctree, width= 100, height= 120, units= 'mm', dpi= 300)
+ggsave(snakemake@output[[1]], plot= ctree, width= 100, height= 120, units= "mm", dpi= 300)
+')
+
+
+circ1= ggtree(hc) +
+theme(legend.position='none') +
+geom_hilight(node=21, fill= colorBlindBlack8[7], alpha=0.5) +
+#geom_cladelabel(21, "Mt", offset= 0.0, barsize=0, angle=0, offset.text= -0.15, hjust=0.5, fontsize=8/.pt) +
+geom_hilight(node=24, fill= colorBlindBlack8[3], alpha=0.5) +
+#geom_cladelabel(24, "Opp.", offset=0.0, barsize=0, angle=0, offset.text=-0.3, hjust=0.5, fontsize=8/.pt) +
+geom_hilight(node=27, fill=colorBlindBlack8[8], alpha=0.5) +
+#geom_cladelabel(27, "Maternal", offset=0.0, barsize=0, angle=0, offset.text=-0.45, hjust=0.5, fontsize=8/.pt) +
+geom_hilight(node=26, fill="grey", alpha=0.5)  +
+#geom_cladelabel(26, "Unclassified", offset=0.0, barsize=0, angle=0, offset.text=-0.3, hjust=0.5, fontsize=8/.pt) +
+theme(plot.margin=margin(0, 0, 0, 0, unit= 'cm'))+
+scale_x_continuous(expand= c(0, NA), limits= c(0, 3.1))
+
+grp= split(d$rsid_lab, d$clust)
+max_beta= max(abs(as.numeric(stack(d)$values)))
+
+
+p1= groupOTU(circ1, grp, 'RSID') +
+aes(color= RSID, angle= 45, hjust= 1) +
+theme(legend.position="none",
+plot.margin=margin(0, 0, 0, 0, unit= 'cm')) +
+scale_colour_manual(values=c(colorBlindBlack8[c(1, 8, 3)], 'grey', colorBlindBlack8[7]), guide= 'none')
+
+
+ctree= gheatmap(p1, df1, offset= 0, width= 0.7,
+        colnames= TRUE, legend_title= "genotype", colnames_offset_y= -1, colnames_angle= 0, font.size= 8/.pt, hjust= 0.5) +
+    geom_tiplab(align = FALSE, linesize= 0, offset= -0.9, size= 8/.pt, hjust= 1) +
+    scale_fill_gradient2(low= colorBlindBlack8[2], high= colorBlindBlack8[4], mid= 'white', guide= F, limits= c(-max_beta, max_beta)) +
+coord_cartesian(clip = 'off') +
+  theme(plot.margin=margin(0, 0, 0, 0.5, unit= 'cm')) +
+  scale_x_reverse(limits= c(2.5, 0))+ 
+coord_flip(clip= 'off')
+  
+ggsave(snakemake@output[[1]], plot= ctree, width= 185, height= 120, units= 'mm', dpi= 300)
+
 
 fwrite(d, snakemake@output[[2]], sep= '\t')
